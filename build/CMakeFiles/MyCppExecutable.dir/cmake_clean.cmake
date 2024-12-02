4ca7956651ae238621e491443dec0f4bc9be93f8
Paulo-Camacho <anthropologyparliament@gmail.com>
2024-12-01 16:41:48 -0800

    initial

---
+++ build/CMakeFiles/MyCppExecutable.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MyCppExecutable.dir/main.cpp.o"
+  "CMakeFiles/MyCppExecutable.dir/main.cpp.o.d"
+  "bin/MyCppExecutable"
+  "bin/MyCppExecutable.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/MyCppExecutable.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
